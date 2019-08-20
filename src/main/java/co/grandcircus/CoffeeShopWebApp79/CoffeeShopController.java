cffc01967c922daa550f3c3828b16e6f9f8584ce
sean <seanhcyang10@gmail.com>
2019-08-20 08:56:46 -0400

    fix stuff again

--- src/main/java/co/grandcircus/CoffeeShopWebApp79/CoffeeShopController.java
+++ src/main/java/co/grandcircus/CoffeeShopWebApp79/CoffeeShopController.java
@@ -1,9 +1,7 @@
 package co.grandcircus.CoffeeShopWebApp79;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
